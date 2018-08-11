f2ddd11aa9578466b6092258a8493cbf1bc7bbfa
Micheal Uchenna Nnani <uchenna.nnani@outlook.com>
2018-08-11 10:24:31 +0100

    fixed imageurl endpoint

--- src/App.js
+++ src/App.js
@@ -101,7 +101,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input})
-      fetch("https://guarded-stream-50690.herokuapp.com//imageurl", {
+      fetch("https://guarded-stream-50690.herokuapp.com/imageurl", {
         method: "post",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
